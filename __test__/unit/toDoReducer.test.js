033c47ffd15707693ead263e6cdab92f0acc9e9f
bigredtiger <simon@bigredtiger.com>
2017-12-15 20:16:49 -0500

    Added cypress for e2e, updated tests for reducer

--- __test__/unit/toDoReducer.test.js
+++ __test__/unit/toDoReducer.test.js
@@ -1,22 +1,36 @@
 import toDoReducer from '../../app/redux/store/reducers/toDoReducer';
+import stateTree from '../../app/redux/store/stateTree';
 
 describe('Todo reducer actions', () => {
     test('It can add item to list', () => {
         const state = { list: [1] };
         const action = { type: 'ADD_TODO', item: 2 };
-        const newState = toDoReducer(state, action);
-        expect(newState.list.length).toEqual(2);
+        const result = toDoReducer(state, action);
+		
+        expect(result.list.length).toEqual(2);
     });
+	
     test('It can remove item from list', () => {
         const state = { list: [1] };
         const action = { type: 'REMOVE_TODO', idx: 0 };
-        const newState = toDoReducer(state, action);
-        expect(newState.list.length).toEqual(0);
+        const result = toDoReducer(state, action);
+		
+        expect(result.list.length).toEqual(0);
     });
+	
     test('It adds the correct item', () => {
         const state = { list: [1] };
         const action = { type: 'ADD_TODO', item: 'simon' };
-        const newState = toDoReducer(state, action);
-        expect(newState.list[newState.list.length - 1]).toEqual('simon');
-    });     
+        const result = toDoReducer(state, action);
+		
+        expect(result.list[result.list.length - 1]).toEqual('simon');
+    });
+
+    test('It will return default state by default', () => {
+		const action = { type: 'foo' };
+        const result = toDoReducer(undefined, action);
+		const expected = stateTree;
+		
+        expect(result).toEqual(expected);
+    });	
 });
--- __test__/unit/utils.test.js
+++ __test__/unit/utils.test.js
@@ -3,6 +3,13 @@ import { computeCount } from '../../app/redux/utils/computeCount';
 describe('Todo utils tests', () => {
     test('It can compute the count of a list', () => {
         const list = [0, 1];
+		
         expect(computeCount(list)).toBe(2);
     });
+	
+	test('It will match snap shot', () => {
+        const list = [0, 1];
+		
+        expect(computeCount(list)).toMatchSnapshot();		
+	});
 });
